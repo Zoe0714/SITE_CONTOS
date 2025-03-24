041e21fcb4d32b3256515c6e8bfeb2d121236c51
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-03-24 10:43:48 -0300

    Add files via upload

--- site_contos.js
+++ site_contos.js
@@ -1,6 +1,6 @@
 const lamp = document.getElementById('conto1');
 
 function EntrarSite(){
-       window.location.href='file:///C:/ZOE/SITE_CONTOS/conto1.html';
+       window.location.href='file:///C:/3TEC/COMPUTACAO_GRAFICA/SITE_CONTOS-main/conto1.html';
 }
 conto1.addEventListener('dblclick',EntrarSite);
